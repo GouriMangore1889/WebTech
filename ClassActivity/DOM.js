489765ac8404edbba87b94159f3380b53d21c363
Gouri Mangore <mangoregouri@gmail.com>
2026-02-05 10:26:29 +0530

    DOM

---
+++ ClassActivity/DOM.js
@@ -0,0 +1,29 @@
+// DOM = Document Object MOdel
+//DOM is bridge between HTML and js
+//It is programming interface
+//when broswer loads an html page and it converts HTML into an Object structure  is called DOM
+//js uses DOM to read change a and delete HTML element
+
+// document.getElementById("title")
+//document.getElementById("title").innerHTML = "<h1>Samiksha</h2>"
+
+
+//DOM Tags = activity 1
+//document.getElementById().class
+//document.getElementById().className
+//document.getElementById().getAttribute
+//document.getElementById().setAttribute
+
+//activity 2
+document.querySelector('h1')
+//gives only one elememt
+document.querySelectorAll('li')
+//gives all list elements
+
+//Acitivty 3 
+//simple project in js 
+
+//DOM manupulation
+  //means changing html by using js
+  //ex.=>show error msg,display success msg,clear input feilds and change color to the red
+   
